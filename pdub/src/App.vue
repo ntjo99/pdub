31ae0a7296186459943dff6c6c091e6d7cb9316a
ntjo99 <142608280+ntjo99@users.noreply.github.com>
2023-08-18 11:16:35 -0400

    Add files via upload

---
+++ pdub/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <BigPapa/>
+</template>
+
+<script>
+import BigPapa from './components/BigPapa.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BigPapa
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pdub/src/components/CommonScreen.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>Yay</p>
+</template>
